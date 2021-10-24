26f96fbf4efe75c04c1622a5d87ce63fba32bf40
mazikdot <mazikdot@rmutsvmail.com>
2021-10-24 22:29:32 +0700

    update token

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -15,7 +15,7 @@
    <br>
    <!-- edit -->
         <form  method="post" class="form" @submit.prevent="handleSubmit">
-            <h1 class="form__title" style="  font-size: 25px;">เข้าสู่ระบบค้นหาเตียงผู้ป่วย</h1>
+            <h1 class="form__title" style="font-size: 25px;">เข้าสู่ระบบค้นหาเตียงผู้ป่วย</h1>
             <div class="form__message form__message--error"></div>
             <div class="form__input-group">
                 <input type="text" class="form__input" name="user_username" v-model="user_username" autofocus placeholder="กรอกบัญชีผู้ใช้" required>
@@ -35,6 +35,8 @@
         </form>
     </div>
     <br><br><br><br>
+    <br><br><br><br>
+ 
     </div>
 </template>
 <script>
@@ -48,10 +50,8 @@ export default {
         }
     },
     methods:{
-// นั้นมีคำสั่งที่ทำงานแบบ asynchronous ก็คือเวลาที่เราสั่งงานอะไรไปแล้วถ้าเป็นงานที่ใช้เวลานาน มันก็จะไล่ไปทำคำสั่งถัดไปเลยโดยไม่ได้รอให้คำสั่งก่อนหน้าทำเสร็จ
       async  handleSubmit(){
           const response = await axios.post('http://localhost:3001/api/login',{
- //   await ใช้เพื่อบอกให้ JavaScript รอจนกว่าคำสั่งนั้นจะเสร็จ ถึงค่อยไปทำงานอันต่อไป โดยฟังก์ชันที่จะมี await อยู่ข้างในได้ต้องประกาศเป็น async เสมอ
               user_username: this.user_username,
               user_passwords: this.user_passwords
           });
